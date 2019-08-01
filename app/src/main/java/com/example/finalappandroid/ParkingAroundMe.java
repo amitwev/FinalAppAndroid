07ae002253bff364763d28a8b4f51b75eb5c7fa9
amitwev <amitwev@gmail.com>
2019-08-01 17:15:29 +0300

    added the screens

---
+++ app/src/main/java/com/example/finalappandroid/ParkingAroundMe.java
@@ -0,0 +1,13 @@
+package com.example.finalappandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParkingAroundMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking_around_me);
+    }
+}
